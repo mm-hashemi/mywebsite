51a695bf2ab0c388a4f0cb2c90b0c42ae63e67bc
mahdi <mmhashemi.9473@gmail.com>
2025-04-18 12:29:57 +0330

    apiveiwChanges

--- home/urls.py
+++ home/urls.py
@@ -3,11 +3,14 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from home.views import ProfileViewSet, SkillViewSet, ProjectViewSet
 
-router = DefaultRouter()
-router.register('profile', ProfileViewSet)
-router.register('skills', SkillViewSet)
-router.register('projects', ProjectViewSet)
+# router = DefaultRouter()
+# router.register('profile', ProfileViewSet)
+# router.register('skills', SkillViewSet)
+# router.register('projects', ProjectViewSet)
 
 urlpatterns = [
-    path('', include(router.urls)),
+   path('profile/', ProfileViewSet.as_view()),
+   path('skills/', SkillViewSet.as_view()),
+   path('projects/', ProjectViewSet.as_view()),
+
 ]
--- home/views.py
+++ home/views.py
@@ -4,20 +4,26 @@ from .serializers import ProfileSerializer, SkillSerializer, ProjectSerializer
 from django.views.decorators.csrf import csrf_exempt
 from django.utils.decorators import method_decorator
 from rest_framework.views import APIView
+from rest_framework.response import Response
 
 @method_decorator(csrf_exempt, name='dispatch')
 class ProfileViewSet(APIView):
-    queryset = Profile.objects.all()
-    serializer_class = ProfileSerializer
+    def get(self, request):
+        profiles= Profile.objects.all()
+        serializer = ProfileSerializer(profiles, many=True)
+        return  Response(serializer.data)
+    
 
 @method_decorator(csrf_exempt, name='dispatch')
 class SkillViewSet(APIView):
-    queryset = Skill.objects.all()
-    serializer_class = SkillSerializer
+    def get(self, request):
+        skills = Skill.objects.all()         
+        serializer = SkillSerializer(skills, many=True)
+        return Response(serializer.data)
 
 @method_decorator(csrf_exempt, name='dispatch')
 class ProjectViewSet(APIView):
-    queryset = Project.objects.all()
-    serializer_class = ProjectSerializer
-
-    
+    def get(self, request):
+        projects = Project.objects.all()
+        serializer = ProjectSerializer(projects, many=True)
+        return Response(serializer.data)
